ef2d92d146eca0513a5d1775aa778cc7ee731a5d
Yuri Baldi <yuri.baldi@unifesp.br>
2024-03-11 18:12:15 -0300

    initial commit

---
+++ Urna Eletronica/urna-eletronica/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>  
+</template>
+
+<script>
+
+import '@/css/global.css';
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>

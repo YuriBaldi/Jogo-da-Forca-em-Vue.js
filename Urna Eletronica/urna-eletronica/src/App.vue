f045ebcbbabd98f9f97bd5878a45e8da182ead5c
Yuri Baldi <yuri.baldi@unifesp.br>
2024-03-22 13:09:12 -0300

    Urna commit

--- Urna Eletronica/urna-eletronica/src/App.vue
+++ Urna Eletronica/urna-eletronica/src/App.vue
@@ -1,18 +1,103 @@
 <template>
-  <div></div>  
+  <div class="app">
+    <div class="urna">
+
+      <tecladoUrna
+      :adicionaNumero="adicionaNumero"
+      />
+
+      <telaUrna
+      :tela="tela"
+      :quantidadeNumeros="quantidadeNumeros"
+      :numeroVoto="numeroVoto"
+
+      />
+
+    </div>
+
+  </div>  
 </template>
 
 <script>
 
 import '@/css/global.css';
+import tecladoUrna from '@/components/tecladoUrna.vue';
+import telaUrna from './components/telaUrna.vue';
 export default {
   name: 'App',
   components: {
+    tecladoUrna,
+    telaUrna
     
+  },
+
+  methods:{
+    adicionaNumero(numero){
+      if(this.numeroVoto.length == this.quantidadeNumeros){
+        return false;
+      }
+      this.numeroVoto += ''+ numero;
+    }
+    
+  },
+  data(){
+    return{
+      tela:'prefeito',
+      numeroVoto:'',
+      quantidadeNumeros: 2,
+      candidato:{},
+      candidatos:{
+            "prefeito":{
+              "01":{
+                "nome": "Ash",
+                "partido": "Pokemon",
+                "imagem": "https://raw.githubusercontent.com/william-costa/wdev-urna-eletronica-resources/master/images/ash.png"
+              },
+              "08":{
+                "nome": "Vegeta",
+                "partido": "Dragon Ball",
+                "imagem": "https://raw.githubusercontent.com/william-costa/wdev-urna-eletronica-resources/master/images/vegeta.png"
+              }
+            },
+            "vereador":{
+                "01234":{
+                  "nome": "Pikachu",
+                  "partido": "Pokemon",
+                  "imagem": "https://raw.githubusercontent.com/william-costa/wdev-urna-eletronica-resources/master/images/pikachu.png"
+                },
+                "08001":{
+                  "nome": "Goku",
+                  "partido": "Dragon Ball",
+                  "imagem": "https://raw.githubusercontent.com/william-costa/wdev-urna-eletronica-resources/master/images/goku.png"
+     }
+  }
+}
+
+      
+    }
   }
 }
 </script>
 
 <style>
+  #app {
+    background-color: var(--background-color);
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+  }
+
+  .urna{
+    width: 1000px;
+    height: 500px;
+    background-color: var(--ballot-box-background-color);
+    padding: 30px;
+    border-radius: 12px;
+    display: flex;
+    justify-content: space-between;
+  }
 
 </style>
---
+++ Urna Eletronica/urna-eletronica/src/components/telaUrna.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="urna-tela">
+
+        <div v-if="tela !='fim'" class="urna-tela-voto">
+            
+            <div class="urba-tela-voto-textos">
+
+                <div class="urna-tela-voto-titulo">Seu voto para:</div>
+                <div class="urna-tela-voto-tipo">{{ tela }}</div>
+                <div class="urna-tela-voto-numeros">
+                    numeros
+                    <div class="urna-tela-voto-numero" v-for="(value,key) in numeroVoto.padEnd(quantidadeNumeros,' ')" :key="key">
+
+                        {{ value }}
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="urna-tela-voto-imagem">
+
+            </div>
+
+            <div class="urna-tela-voto-instrucoes">
+
+            </div>
+        
+        
+        </div>
+
+
+        <div v-if="tela =='fim'" class="urna-tela-fim">
+          
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+export default{
+    name:'telaUrna',
+    props:{
+        tela: String,
+        numeroVoto: String,
+        quantidadeNumeros: Number,
+    }
+}
+
+</script>
+
+<style>
+
+.urna-tela{
+    width: 55%;
+    height: 100%;
+    background-color: var(--ballot-box-screen-color);
+    border-radius: 5px;
+    border: 2px solid var(--light-border-color);
+    padding:20px;
+    color: var(--dark=text-color);
+}
+
+.urna-tela-voto-titulo{
+        font-size: 20px;
+}
+
+.urna-tela-voto-tipo{
+    font-size: 30px;
+    font-weight: bold;
+    margin: 20px 0;
+    text-transform: uppercase;
+}
+
+.urna-tela-voto-numeros{
+    display: flex;
+    align-items: center;
+}
+
+.urna-tela-voto-numero{
+    width: 45px;
+    height: 55px;
+    border: 1px solid var(--dark-border-color);
+    margin-left: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 30px;
+}
+</style>
